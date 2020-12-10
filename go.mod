2d90b10731f8881800cfcac7df2ef81bf5b2cf0e
HD Moore <x@hdm.io>
2020-12-10 12:11:12 -0600

    rework examples, wip for scan scheduler

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/antihax/optional v1.0.0
 	github.com/aws/aws-sdk-go v1.36.5
+	github.com/gofrs/uuid v3.3.0+incompatible
 	github.com/golang/protobuf v1.4.3 // indirect
 	golang.org/x/net v0.0.0-20201209123823-ac852fbbde11 // indirect
 	golang.org/x/oauth2 v0.0.0-20201208152858-08078c50e5b5
---
+++ go/rumble-client/examples/scan-all-sites/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	rc "github.com/RumbleDiscovery/rumble-api/go/rumble-client"
+	"github.com/gofrs/uuid"
+)
+
+func main() {
+	var err error
+
+	ctx := context.Background()
+	client := rc.NewClient(&rc.Config{})
+	cnf := client.GetConfig()
+	if len(cnf.DefaultHeader) == 0 {
+		log.Printf("Set the RUMBLE_API_KEY environment variable")
+		return
+	}
+
+	// Build a list of connected agents
+	getAgentsReq := client.OrganizationApi.GetAgents(ctx)
+	agents, _, err := getAgentsReq.Execute()
+	if err != nil {
+		log.Fatalf("failed to get agent list %s", err)
+	}
+
+	// Build a map of SiteID to AgentID
+	siteAgentMap := make(map[string]string)
+	for _, agent := range agents {
+		if !agent.GetConnected() {
+			log.Printf("warning: agent %s [%s] is offline and will be ignored", agent.GetName(), agent.GetId())
+			continue
+		}
+		if agent.GetSiteId() == uuid.Nil.String() {
+			log.Printf("warning: agent %s [%s] is not assigned to a specific site and will be ignored", agent.GetName(), agent.GetId())
+			continue
+		}
+		if v, ok := siteAgentMap[agent.GetSiteId()]; ok {
+			log.Printf("warning: agent %s [%s] is ignored as %s is already assigned to this site", agent.GetName(), agent.GetId(), v)
+			continue
+		}
+		siteAgentMap[agent.GetSiteId()] = agent.GetId()
+	}
+
+	// Build a list of recurring tasks
+	getTasksReq := client.OrganizationApi.GetTasks(ctx)
+	tasks, _, err := getTasksReq.Execute()
+	if err != nil {
+		log.Fatalf("failed to get task list %s", err)
+	}
+
+	// Build a map of SiteID to TaskID
+	siteTaskMap := make(map[string]string)
+	for _, task := range tasks {
+		if !task.GetRecur() {
+			continue
+		}
+		if task.GetType() != "scan" {
+			continue
+		}
+		siteTaskMap[task.GetSiteId()] = task.GetId()
+	}
+
+	// Build a map of SiteID to TaskID
+	log.Printf("siteMap: %#v", siteAgentMap)
+	log.Printf("taskMap: %#v", siteTaskMap)
+
+}
