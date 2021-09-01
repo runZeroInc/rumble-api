0e9106bf1b417f5b1924a33c5f74462a85da9603
mathew murphy <mathew@rumble.run>
2021-09-01 09:31:04 -0500

    Better logging, make sure zip is rebuilt

--- offline-docs/splice.js
+++ offline-docs/splice.js
@@ -6,11 +6,11 @@ const specYAML = 'rumble-api.yml';
 
 try {
   const doc = yaml.load(fs.readFileSync(specYAML, 'utf8'));
-  const jsondata = JSON.stringify(doc, null, 2);
+  const jsonData = JSON.stringify(doc, null, 2);
   const index = fs.readFileSync(indexFile, 'utf8');
-  const newindex = index.replace(/url:.*swagger.io.*\.json",/m, `  spec: ${jsondata},`);
-  console.log(newindex);
-  fs.writeFileSync(indexFile, newindex, 'utf8');
+  const newIndex = index.replace(/url:.*swagger.io.*\.json",/m, `  spec: ${jsonData},`);
+  fs.writeFileSync(indexFile, newIndex, 'utf8');
+  console.log(`Updated ${indexFile}`);
 } catch (e) {
   console.log(e);
 }
