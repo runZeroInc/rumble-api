8cbd343a5ce730a2bb7d0b4d927b1b5e4653c88a
mathew murphy <mathew@rumble.run>
2022-01-21 09:21:21 -0600

    Fix PDF documentation
     - Add sed postprocessing of HTML to work around openapi-generator
       bug 11292
     - Remove unused files
     - Update openapi-generator wrapper version

--- pdfgen/makepdf.sh
+++ pdfgen/makepdf.sh
@@ -1,2 +1,5 @@
+cd $(git rev-parse --show-toplevel)
 npx openapi-generator-cli generate -i rumble-api.yml -g html -o pdfgen/html -c pdfgen/config.yaml
+# Work around https://github.com/OpenAPITools/openapi-generator/issues/11292
+sed -i '{ /<div class="param">.*Minus/s/Minus/-/g }' pdfgen/html/index.html
 node pdfgen/makepdf.js
